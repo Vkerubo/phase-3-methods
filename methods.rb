c563065011d6dc2cbf2a26790d400c3aec8378b3
Valentine <kerubovalentine1@gmail.com>
2023-05-16 06:52:01 +0300

    finished assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+def greet_programmer
+    pp "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    pp "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default name = "programmer"
+    pp "Hello, #{name}!"
+end
+greet_with_default "Naureen"
+
+def add num1, num2
+    num1 + num2
+end
+add 3, 7
+
+def halve num
+    if num.class != Integer
+        return nil
+    end
+    num / 2
+end
+halve 2
+    
+    
